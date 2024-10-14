c18ae7e68129e9afa2e1a6ea523d7e7102e0976e
Toni <vicerust@gmail.com>
2024-10-13 21:15:50 -0400

    test minimal stonecutter setup

--- src/main/java/toni/examplemod/ExampleMod.java
+++ src/main/java/toni/examplemod/ExampleMod.java
@@ -1,49 +1,36 @@
 package toni.examplemod;
 
-import toni.examplemod.foundation.config.AllConfigs;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+//? if fabric {
+/*import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.ModInitializer;
+*///?}
 
-#if FABRIC
-    import net.fabricmc.api.ClientModInitializer;
-    import net.fabricmc.api.ModInitializer;
-    #if after_21_1
-    import fuzs.forgeconfigapiport.fabric.api.neoforge.v4.NeoForgeConfigRegistry;
-    import fuzs.forgeconfigapiport.fabric.api.neoforge.v4.client.ConfigScreenFactoryRegistry;
-    import net.neoforged.neoforge.client.gui.ConfigurationScreen;
-    #endif
-
-    #if current_20_1
-    import fuzs.forgeconfigapiport.api.config.v2.ForgeConfigRegistry;
-    #endif
-#endif
-
-#if FORGE
+//? if forge {
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-#endif
+//?}
 
 
-#if NEO
-import net.neoforged.fml.common.Mod;
+//? if neoforge {
+/*import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
 import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.EventBusSubscriber;
-import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
-import net.neoforged.neoforge.client.gui.ConfigurationScreen;
-#endif
+*///?}
 
 
-#if FORGELIKE
+//? if forge || neoforge {
 @Mod("example_mod")
-#endif
+//?}
 public class ExampleMod #if FABRIC implements ModInitializer, ClientModInitializer #endif
 {
     public static final String MODNAME = "Example Mod";
@@ -51,52 +38,34 @@ public class ExampleMod #if FABRIC implements ModInitializer, ClientModInitializ
     public static final Logger LOGGER = LogManager.getLogger(MODNAME);
 
     public ExampleMod(#if NEO IEventBus modEventBus, ModContainer modContainer #endif) {
-        #if FORGE
+        //? if forge {
         var context = FMLJavaModLoadingContext.get();
         var modEventBus = context.getModEventBus();
-        #endif
+        //?}
 
-        #if FORGELIKE
+        //? if forge || neoforge {
         modEventBus.addListener(this::commonSetup);
         modEventBus.addListener(this::clientSetup);
-
-        AllConfigs.register((type, spec) -> {
-            #if FORGE
-            ModLoadingContext.get().registerConfig(type, spec);
-            #elif NEO
-            modContainer.registerConfig(type, spec);
-            modContainer.registerExtensionPoint(IConfigScreenFactory.class, ConfigurationScreen::new);
-            #endif
-        });
-        #endif
+        //?}
     }
 
-
-    #if FABRIC @Override #endif
+    //? if fabric {
+    /*@Override
+    *///?}
     public void onInitialize() {
-        #if FABRIC
-            AllConfigs.register((type, spec) -> {
-                #if AFTER_21_1
-                NeoForgeConfigRegistry.INSTANCE.register(ExampleMod.ID, type, spec);
-                #else
-                ForgeConfigRegistry.INSTANCE.register(ExampleMod.ID, type, spec);
-                #endif
-            });
-        #endif
+
     }
 
-    #if FABRIC @Override #endif
+    //? if fabric {
+    /*@Override
+     *///?}
     public void onInitializeClient() {
-        #if AFTER_21_1
-            #if FABRIC
-            ConfigScreenFactoryRegistry.INSTANCE.register(ExampleMod.ID, ConfigurationScreen::new);
-            #endif
-        #endif
+
     }
 
     // Forg event stubs to call the Fabric initialize methods, and set up cloth config screen
-    #if FORGELIKE
+    //? if forge || neoforge {
     public void commonSetup(FMLCommonSetupEvent event) { onInitialize(); }
     public void clientSetup(FMLClientSetupEvent event) { onInitializeClient(); }
-    #endif
+    //?}
 }
